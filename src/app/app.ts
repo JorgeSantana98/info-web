632451948d7006903de7cdb961ff0ccc91fbbb01
JorgeSantana98 <jorgesantana105@gmail.com>
2025-06-17 15:30:52 -0500

    feat: agrega inicio de la pagina

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBar } from './shared/nav-bar/nav-bar';
 import { Footer } from './shared/footer/footer';
+import { HomePage } from './pages/home-page/home-page';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavBar, Footer],
+  imports: [RouterOutlet, NavBar, Footer, HomePage],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
